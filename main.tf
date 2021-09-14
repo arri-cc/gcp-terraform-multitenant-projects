66eb69087d7738269e202463b338e096400ddca3
Arri Rucker <arri@arri.io>
2021-09-14 00:25:05 -0400

    output var

--- main.tf
+++ main.tf
@@ -22,6 +22,23 @@ resource "google_cloud_run_service" "default" {
   }
 }
 
-output "service_url" {
-  value = google_cloud_run_service.default.url
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+output "url" {
+  value = google_cloud_run_service.default.status[0].url
 }
